a5b8367bdc19ad68372397e9333cce513c86dca9
Ludovico Zarrelli <ludovico.zarr@gmail.com>
2022-06-26 09:38:17 +0200

    improving coverage for JSON

--- src/test/java/com/alibaba/json/bvt/parser/JSONArrayParseTestParameterized.java
+++ src/test/java/com/alibaba/json/bvt/parser/JSONArrayParseTestParameterized.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import com.alibaba.fastjson.parser.Feature;
+import com.sun.jdi.Type;
 import org.junit.Assert;
 import junit.framework.TestCase;
 
@@ -30,33 +32,42 @@ public class JSONArrayParseTestParameterized extends TestCase {
 
     private String text;
     private TypeReference<List<Map<String, Integer>>> typeReference;
+    private Feature feature;
 
-    public JSONArrayParseTestParameterized(String inputString, TypeReference<List<Map<String, Integer>>> typeReference) {
-        configure(inputString,typeReference);
+    public JSONArrayParseTestParameterized(String inputString, TypeReference<List<Map<String, Integer>>> typeReference
+    , Feature feature) {
+        configure(inputString,typeReference, feature);
     }
 
-    private void configure(String inputString,  TypeReference<List<Map<String, Integer>>> typeReference) {
+    private void configure(String inputString,  TypeReference<List<Map<String, Integer>>> typeReference, Feature feature) {
        this.text = inputString;
        this.typeReference = typeReference;
+       this.feature = feature;
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> getParameters() {
         return Arrays.asList(new Object[][] {
-                {"[{id:123}, {}]", new TypeReference<List<Map<String, Integer>>>() {}}
                 // inputString		//TypeReference
+                {"[{id:123}, {}]", new TypeReference<List<Map<String, Integer>>>() {}, Feature.OrderedField},
+
+                {null, new TypeReference<List<Map<String, Integer>>>() {}, Feature.OrderedField},
+                {"", new TypeReference<List<Map<String, Integer>>>() {}, Feature.OrderedField}
         });
     }
 
     @Test
     public void test_array() {
-        List<Map<String, Integer>> array = JSON.parseObject(text, typeReference);
+        List<Map<String, Integer>> array = JSON.parseObject(text, typeReference, feature);
 
-        String[] tokens = text.split("}");
-        String[] tokens2= tokens[0].split(":");
+        if( this.text == null || this.text.isEmpty()) Assert.assertNull(array);
+        else {
+            String[] tokens = text.split("}");
+            String[] tokens2 = tokens[0].split(":");
 
-        Assert.assertEquals( tokens.length - 1, array.size());
-        Map<String, Integer> map  = array.get(0);
-        Assert.assertEquals(Integer.parseInt(tokens2[tokens2.length - 1]), map.get("id").intValue());
+            Assert.assertEquals(tokens.length - 1, array.size());
+            Map<String, Integer> map = array.get(0);
+            Assert.assertEquals(Integer.parseInt(tokens2[tokens2.length - 1]), map.get("id").intValue());
+        }
     }
 }
